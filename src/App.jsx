ee3349e16c6c14807cb530e1b3213303a255e7c8
João Maia <15joaomaia@gmail.com>
2022-03-31 11:37:06 -0300

    projeto iniciado e estruturado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, Fragment } from 'react'
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
